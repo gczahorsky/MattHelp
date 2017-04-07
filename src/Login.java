8dd9761d848b2c97110fe50d39aebe6525a67f2a
Grant Zahorsky <grantzahorsky@grantmac.dyn.wpi.edu>
2017-04-07 10:14:54 -0400

    Initial commit

---
+++ src/Login.java
@@ -0,0 +1,13 @@
+/**
+ * Created by grantzahorsky on 4/6/17.
+ */
+public class Login {
+    public Login() {
+    }
+
+    public void loginFunction() {
+        System.out.println("FUCK YEAH");
+    }
+
+
+}
---
+++ src/com/Servlet2.java
@@ -0,0 +1,23 @@
+package com;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
+
+/**
+ * Created by grantzahorsky on 4/7/17.
+ */
+@WebServlet(name = "Servlet2")
+public class Servlet2 extends HttpServlet {
+
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        if (request.getParameter("button1") != null) {
+            System.out.println("WOO");
+            System.out.flush();
+        }
+    }
+}
